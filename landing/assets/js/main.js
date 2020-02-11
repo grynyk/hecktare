8c5c279af58a8530ce9f5dc3080df315bfda76d5
Danylo Grynyk <danylo.grynyk@globallogic.com>
2020-02-12 00:18:51 +0100

    slick slider integrated

--- landing/assets/js/main.js
+++ landing/assets/js/main.js
@@ -78,5 +78,27 @@ $(function() {
       }
     });
   });
+
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    focusOnSelect: true
+  });
+ 
+  $('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slideno - 1);
+  });
   new WOW().init();
 });
